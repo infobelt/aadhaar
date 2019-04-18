bf35c0f5e030901693f4d48a3fa3d08749786e98
Philip Dodds <philip.dodds@me.com>
2019-04-18 13:25:27 -0400

    Remove annotations since they aren't needed

--- aadhaar-example/src/test/java/com/infobelt/aadhaar/example/domain/WidgetEntityTest.java
+++ aadhaar-example/src/test/java/com/infobelt/aadhaar/example/domain/WidgetEntityTest.java
@@ -1,14 +1,7 @@
 package com.infobelt.aadhaar.example.domain;
 
 import com.infobelt.aadhaar.test.AbstractEntityTestBase;
-import lombok.extern.slf4j.Slf4j;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@Slf4j
-@SpringBootTest
-@RunWith(SpringRunner.class)
 public class WidgetEntityTest extends AbstractEntityTestBase<Widget> {
 
     @Override
--- aadhaar-example/src/test/java/com/infobelt/aadhaar/example/web/WidgetResourceTest.java
+++ aadhaar-example/src/test/java/com/infobelt/aadhaar/example/web/WidgetResourceTest.java
@@ -2,14 +2,7 @@ package com.infobelt.aadhaar.example.web;
 
 import com.infobelt.aadhaar.example.domain.Widget;
 import com.infobelt.aadhaar.test.AbstractYamlResourceTestBase;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
-@RunWith(SpringRunner.class)
-@AutoConfigureMockMvc
 public class WidgetResourceTest extends AbstractYamlResourceTestBase {
 
     @Override
