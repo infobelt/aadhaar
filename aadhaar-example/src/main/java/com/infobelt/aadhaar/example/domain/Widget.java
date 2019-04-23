d7021fcaec470b526e1fc33cb40ebec61fed5b46
Philip Dodds <philip.dodds@me.com>
2019-04-23 12:23:02 -0400

    Fix up use of @Data and include note

--- aadhaar-example/src/main/java/com/infobelt/aadhaar/example/domain/Widget.java
+++ aadhaar-example/src/main/java/com/infobelt/aadhaar/example/domain/Widget.java
@@ -1,11 +1,15 @@
 package com.infobelt.aadhaar.example.domain;
 
 import com.infobelt.aadhaar.domain.AbstractEntity;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 
-@Data
+// Note don't use @Data since our abstract entity implements hashCode and equals and we don't want to
+// override it
+@Getter
+@Setter
 @Entity
 public class Widget extends AbstractEntity {
 
