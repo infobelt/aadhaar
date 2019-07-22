340ba77eeaa6cfcdcfede64d43e3575f655f8d79
superbug74 <sendtolecia@gmail.com>
2019-07-22 07:46:12 -0400

    altering date decoding to simplify to '+'

--- aadhaar-spring-boot/src/main/java/com/infobelt/aadhaar/utils/SqlUtil.java
+++ aadhaar-spring-boot/src/main/java/com/infobelt/aadhaar/utils/SqlUtil.java
@@ -7,8 +7,6 @@ import org.springframework.data.domain.Pageable;
 import javax.persistence.EntityManager;
 import javax.persistence.FlushModeType;
 import javax.persistence.Query;
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
 import java.time.LocalDate;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
@@ -109,13 +107,7 @@ public class SqlUtil {
 
     private static String convertDateForSearch(String dateString){
         //First, decode date - date is encoded to prevent the lost of '+'
-        try {
-            dateString = URLDecoder.decode(dateString, "UTF-8");
-        }
-        catch(UnsupportedEncodingException e){
-            return null;
-        }
-
+        dateString = dateString.replace("%2B", "+");
         try{
             ZonedDateTime parsedVal;
             parsedVal = ZonedDateTime.parse(dateString);
